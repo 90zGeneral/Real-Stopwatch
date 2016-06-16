03b0d7b341d91bed31837cd64379be8b0394ef22
Roydon Jeffrey <brushdam14823@gmail.com>
2016-06-15 20:05:11 -0400

    Added a table view and trying to get the button images to work accordingly

--- Real Stopwatch/ViewController.swift
+++ Real Stopwatch/ViewController.swift
@@ -9,6 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate {
+    
+    var lapTime = [Double]()
+    
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+       return lapTime.count
+        
+    }
+    
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = String(lapTime[indexPath.row])
+        return cell
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
