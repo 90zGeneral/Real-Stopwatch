9a02b6a34215b31fd6666a3e57bbf4a1d94acd3a
Roydon Jeffrey <brushdam14823@gmail.com>
2016-06-17 15:11:16 -0400

    Completed the app interface and started writing some code for test driving

--- Real Stopwatch/ViewController.swift
+++ Real Stopwatch/ViewController.swift
@@ -8,25 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate {
+//Allows the view controller to be responsible for the table view
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    var lapTime = [Double]()
+//    var lapTime = [Double]()
+//    var timer = NSTimer()
     
-    var timer = NSTimer()
+    //Link all the necessary UI object as outlets and actions
+    @IBOutlet var timerLabel: UILabel!
+    @IBOutlet var lapsTableView: UITableView!
+    @IBOutlet var startStopButton: UIButton!
+    @IBOutlet var lapResetButton: UIButton!
     
-    
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    @IBAction func startStop(sender: AnyObject) {
         
-       return lapTime.count
+        print("Firing")
         
     }
     
-
+    @IBAction func lapReset(sender: AnyObject) {
+        
+        print("Working")
+        
+    }
+    
+    //Determines how many rows the table view should have
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+       return 3
+        
+    }
+    
+    //Responsible for the content and values within the cells
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
-        cell.textLabel?.text = String(lapTime[indexPath.row])
+        //Use Value1 to align "Lap" on the left side of the cell and it value on the right side of the cell
+        let cell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: "Cell")
+        cell.backgroundColor = self.view.backgroundColor
+        cell.textLabel?.text = "Lap"
+        cell.detailTextLabel?.text = "00:00:00"
         return cell
         
     }
