e164c74316fd8a4c9fceef14605707aa12c3abaa
Roydon Jeffrey <brushdam14823@gmail.com>
2016-06-18 21:39:56 -0400

    Finished the functionalities of the start/stop button but not yet the lap/reset button.

--- Real Stopwatch/ViewController.swift
+++ Real Stopwatch/ViewController.swift
@@ -11,24 +11,90 @@ import UIKit
 //Allows the view controller to be responsible for the table view
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-//    var lapTime = [Double]()
-//    var timer = NSTimer()
+    //Create all the necessary variables for each component of the app
+    var timer = NSTimer()
+    var minutes: Int = 0
+    var seconds: Int = 0
+    var fractions: Int = 0
+    
+    //Boolean variables to tell whether the app has started and if any laps has been added
+    var startStopwatch: Bool = false
+    var addLap: Bool = false
     
     //Link all the necessary UI object as outlets and actions
-    @IBOutlet var timerLabel: UILabel!
+    @IBOutlet var stopwatchLabel: UILabel!
     @IBOutlet var lapsTableView: UITableView!
     @IBOutlet var startStopButton: UIButton!
     @IBOutlet var lapResetButton: UIButton!
     
+    //To determine what the startStop button should do when it is clicked
     @IBAction func startStop(sender: AnyObject) {
         
-        print("Firing")
+        //Only run this statement if startStopwatch is false
+        if !startStopwatch {
+            
+            //Timer to call the updateStopwatch function every 0.01 second
+            timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: #selector(ViewController.updateStopwatch), userInfo: nil, repeats: true)
+            
+            //Reset startStopwatch and addLap to true
+            startStopwatch = true
+            addLap = true
+            
+            //To reset the images of each button while stopwatch is running
+            startStopButton.setImage(UIImage(named: "stopButon.png"), forState: UIControlState.Normal)
+            lapResetButton.setImage(UIImage(named: "lapButton.png"), forState: UIControlState.Normal)
+    
+        }else {
+            //Stop the timer
+            timer.invalidate()
+            
+            //Reset startStopwatch and addLap to false
+            startStopwatch = false
+            addLap = false
+            
+            //To reset the images of each button while stopwatch has been stopped
+            startStopButton.setImage(UIImage(named: "startButton.png"), forState: .Normal)
+            lapResetButton.setImage(UIImage(named: "resetButton.png"), forState: .Normal)
+    
+        }
         
     }
     
     @IBAction func lapReset(sender: AnyObject) {
+    
         
-        print("Working")
+        
+    }
+    
+    //Function to be called when startStop button is clicked and startStopwatch is set to false
+    func updateStopwatch() {
+        
+        //Increment fractions by 1
+        fractions += 1
+        
+        //Check is fractions is equal to 100 to increment seconds by 1 and reset fraction back to 0
+        if fractions == 100 {
+            
+            seconds += 1
+            fractions = 0
+            
+        }
+        
+        //Check is seconds is equal to 60 to increment minutes and reset seconds back to 0
+        if seconds == 60 {
+            
+            minutes += 1
+            seconds = 0
+            
+        }
+        
+        //To allow fractions, seconds, and minutes to display a double digit figure at all times
+        let fractionsString = fractions > 9 ? "\(fractions)" : "0\(fractions)"
+        let secondsString = seconds > 9 ? "\(seconds)" : "0\(seconds)"
+        let minutesString = minutes > 9 ? "\(minutes)" : "0\(minutes)"
+        
+        //Update the stopwatchLabel
+        stopwatchLabel.text = "\(minutesString):\(secondsString).\(fractionsString)"
         
     }
     
@@ -42,11 +108,11 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     //Responsible for the content and values within the cells
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        //Use Value1 to align "Lap" on the left side of the cell and it value on the right side of the cell
-        let cell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: "Cell")
+        //Use Value1 to align "Lap" on the left side of the cell and it value on the right side of the cell and set background color of the cells to the background color of the viewController
+        let cell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: "Cell")  //Same reuseIdentifier as on Main.storyboard 
         cell.backgroundColor = self.view.backgroundColor
-        cell.textLabel?.text = "Lap"
-        cell.detailTextLabel?.text = "00:00:00"
+        cell.textLabel?.text = "Lap" //Cell text value
+        cell.detailTextLabel?.text = "00:00:00"   //Cell detail value
         return cell
         
     }
@@ -54,6 +120,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //To determine what value the stopwatch label takes when the app is first loaded
+        stopwatchLabel.text = "00:00.00"
+        
     }
 
     override func didReceiveMemoryWarning() {
